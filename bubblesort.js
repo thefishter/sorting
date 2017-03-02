ac83e720f5345597b20d576eb073496b709f563e
Nicole Fish <nicole.fish@gmail.com>
2017-03-02 15:35:53 -0500

    fish refactor pre-solution vid

--- bubblesort.js
+++ bubblesort.js
@@ -6,32 +6,19 @@ function compare(arr, ind1, ind2) {
 }
 
 function swap(arr, ind1, ind2) {
-	// create temp storage
-	// save arr[ind1] into temp
 	var tmp = arr[ind1];
-	// put arr[ind2] into arr[ind1]
 	arr[ind1] = arr[ind2];
-	// put temp into arr[ind2]
 	arr[ind2] = tmp;
 	swaps++;
-   
-
 }
 
 function bubbleSort(arr) {
 	var len = arr.length;
 	do {
-        console.log('Restarting sort');
 		swaps = 0;
-		// for the length of the array
 		for (var i = 0; i < len-1; i++) {
-			// compare first two elts
-			console.log("compared", arr[i], arr[i + 1]);
             compare(arr, i, i+1);
-             console.log("swaps: ", swaps);
-			// swap if necessary, swaps++
-			// increment elt indices
 		}
-    }while (swaps > 0)
+    } while (swaps > 0)
 	return arr;
 }
